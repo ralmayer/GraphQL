7057a6662094c934d6d863aa10ecaaae20ab9f9f
Ral Mayer <llev4n@gmail.com>
2019-12-29 14:04:01 +0400

    some changes

--- index.js
+++ index.js
@@ -9,13 +9,29 @@ const db = {
     users: [
         {id: "1", name: 'Alex', email: 'alex@gmail.com'},
         {id: "2", name: 'Greg', email: 'greg@gmail.com'}
+    ],
+    messages: [
+        {id: "1", userId: "1", body: "Hey", createdAt: Date.now()},
+        {id: "2", userId: "2", body: "Aye", createdAt: Date.now()},
+        {id: "3", userId: "1", body: "What's up?", createdAt: Date.now()}
     ]
+}
+
+class User {
+    constructor(user) {
+        Object.assign(this, user)
+    }
+
+    messages () {
+        return db.messages.filter(message => message.userId === this.id)
+    }
 }
 
 const schema = buildSchema(`   
     type Query {
         users: [User!]!
         user(id: ID!): User
+        messages: [Message!]!
     }
 
     type Mutation {
@@ -27,12 +43,20 @@ const schema = buildSchema(`
         name: String
         email: String!
         avatarUrl: String
+        messages: [Message!]!
+    }
+
+    type Message {
+        id: ID!
+        body: String!
+        createdAt: String
     }
 `);
 
 const rootValue = {
-    users: () => db.users,
+    users: () => db.users.map(user => new User(user)),
     user: args => db.users.find(user => user.id === args.id),
+    messages: () => db.messages,
     addUser: ({email, name}) => {
         const user = {
             id: crypto.randomBytes(10).toString('hex'),
